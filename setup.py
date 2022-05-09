d70749664461ee7d3539eca5a1971586f192c236
Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
2022-05-09 15:48:03 -0400

    Bump to v1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-higherorderaccurategradient',
-    version='1.1.1',
+    version='1.1.2',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
