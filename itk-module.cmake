053938256c2f3ba1100cff1edc478061097345f7
Tom Birdsong <tom.birdsong@kitware.com>
2022-05-31 11:27:36 -0400

    ENH: Bump ITK and replace http with https using script

--- itk-module.cmake
+++ itk-module.cmake
@@ -3,8 +3,8 @@ set(DOCUMENTATION "This module contains a filter to compute higher order
 accurate numerical derivatives and gradients from an input scalar image.
 field from a displacement field image.
 Higher Order Accurate Derivative and Gradient Calculation in ITK
-http://www.insight-journal.org/browse/publication/775
-http://hdl.handle.net/10380/3231
+https://www.insight-journal.org/browse/publication/775
+https://hdl.handle.net/10380/3231
 ")
 
 itk_module(HigherOrderAccurateGradient
